0923de0cc07b76c7551755d7d19f4b8e590bab9e
antonio-datahack <antonio.datahack@gmail.com>
2022-07-13 23:21:28 +0200

    iteration 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+//import { useState, React } from "react";
 import './App.css';
+// import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import Foods from './foods.json';
+import "bootstrap/dist/css/bootstrap.min.css";
+import FoodBox from "./components/FoodBox";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [foods, setfoods] = useState([...Foods]);
+  return <div className="App">
+    <h2>Food List</h2>
+    {/* {foods.map(food => (
+      <div key={food._id}>
+        <p> {food.name} </p>
+        <img src={food.image} width="200px" />
+      </div>))
+      } */}
+      <FoodBox food={ {
+          name: "Orange",
+          calories: 85,
+          image: "https://i.imgur.com/abKGOcv.jpg",
+          servings: 1
+        }} />
+      
+
+  </div>;
 }
 
 export default App;
